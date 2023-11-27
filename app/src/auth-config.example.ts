3f00c8caca07da46d03c700515b75f0b3c4d1966
Kikuo Emoto <kemoto@codemonger.io>
2023-11-27 15:26:40 +0900

    chore(app): update auth-config example
    - Adds `isCognito` and `credentialsApiUrl`.

--- app/src/auth-config.example.ts
+++ app/src/auth-config.example.ts
@@ -1,1 +1,4 @@
-export const userPoolClientId = 'Cognito user pool client ID for your app';
+export const isCognito = false;
+export const userPoolClientId = 'Cognito user pool client ID for your app. Has no effect if isCognito is false';
+// URL of the credentials API
+export const credentialsApiUrl = 'http://localhost:3000/auth/';
