821f33c003544d92864f4a1dc3d2e80109812101
Kikuo Emoto <kemoto@codemonger.io>
2023-11-30 23:15:04 +0900

    chore(app): update auth-config example

--- app/src/auth-config.example.ts
+++ app/src/auth-config.example.ts
@@ -1,4 +1,6 @@
 export const isCognito = false;
 export const userPoolClientId = 'Cognito user pool client ID for your app. Has no effect if isCognito is false';
-// URL of the credentials API
-export const credentialsApiUrl = 'http://localhost:3000/auth/';
+// URL of the credentials API (for rp-server)
+export const credentialsApiUrl = '/auth/';
+// URL of the credentials API (for app distribution)
+// export const credentialsApiUrl = '/auth/credentials/';
