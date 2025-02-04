fbed3c0ff5db851a7e3061bd4f027847c58e1f72
Kikuo Emoto <kemoto@codemonger.io>
2025-02-04 19:03:17 +0900

    sytle(cdk): change indentation of lib/session-store.ts

--- cdk/lib/session-store.ts
+++ cdk/lib/session-store.ts
@@ -3,50 +3,50 @@ import { Construct } from 'constructs';
 
 /** CDK construct that provisions the DynamoDB table for sessions. */
 export class SessionStore extends Construct {
-    /**
-     * DynamoDB table that stores sessions.
-     *
-     * ## Keys and attributes
-     *
-     * - Partition key: `pk`
-     * - No sort key
-     * - Time to live attribute: `ttl`
-     *
-     * ### User registration session
-     *
-     * - `pk`: "registration#<session ID>"
-     * - `ttl`: 60 seconds after the session was created
-     * - `userId`: unique user ID
-     * - `userInfo`:
-     *     - `username`: unique username
-     *     - `displayName`: display name
-     * - `state`: serialized internal state
-     *
-     * ### User authentication session with a user-side discoverable credential
-     *
-     * - `pk`: "discoverable#<challenge>"
-     *     - `<challenge>` is the "base64url"-encoded challenge
-     * - `ttl`: 60 seconds after the session was created
-     * - `state`: serialized internal state
-     */
-    readonly sessionTable: dynamodb.TableV2;
+  /**
+   * DynamoDB table that stores sessions.
+   *
+   * ## Keys and attributes
+   *
+   * - Partition key: `pk`
+   * - No sort key
+   * - Time to live attribute: `ttl`
+   *
+   * ### User registration session
+   *
+   * - `pk`: "registration#<session ID>"
+   * - `ttl`: 60 seconds after the session was created
+   * - `userId`: unique user ID
+   * - `userInfo`:
+   *   - `username`: unique username
+   *   - `displayName`: display name
+   * - `state`: serialized internal state
+   *
+   * ### User authentication session with a user-side discoverable credential
+   *
+   * - `pk`: "discoverable#<challenge>"
+   *   - `<challenge>` is the "base64url"-encoded challenge
+   * - `ttl`: 60 seconds after the session was created
+   * - `state`: serialized internal state
+   */
+  readonly sessionTable: dynamodb.TableV2;
 
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-        this.sessionTable = new dynamodb.TableV2(this, 'SessionTable', {
-            partitionKey: {
-                name: 'pk',
-                type: dynamodb.AttributeType.STRING,
-            },
-            timeToLiveAttribute: 'ttl',
-            billing: dynamodb.Billing.provisioned({
-                readCapacity: dynamodb.Capacity.fixed(1),
-                writeCapacity: dynamodb.Capacity.autoscaled({
-                    maxCapacity: 1,
-                }),
-            }),
-            removalPolicy: RemovalPolicy.DESTROY,
-        });
-    }
+    this.sessionTable = new dynamodb.TableV2(this, 'SessionTable', {
+      partitionKey: {
+        name: 'pk',
+        type: dynamodb.AttributeType.STRING,
+      },
+      timeToLiveAttribute: 'ttl',
+      billing: dynamodb.Billing.provisioned({
+        readCapacity: dynamodb.Capacity.fixed(1),
+        writeCapacity: dynamodb.Capacity.autoscaled({
+          maxCapacity: 1,
+        }),
+      }),
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+  }
 }
