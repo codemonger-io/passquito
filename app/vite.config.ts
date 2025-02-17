0df6ae8b6793fcd3dc6f9847614ba0127020b7c2
Kikuo Emoto <kemoto@codemonger.io>
2025-02-17 18:51:04 +0900

    chore(app): evaluate dotenv (.env) files in Vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/app/',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.PASSQUITO_DEMO_BASE_URL || '/app/',
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
