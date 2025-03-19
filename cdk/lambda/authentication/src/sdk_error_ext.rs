382e3242b94f5951a47ecef8c52a74ec2dd68aed
Kikuo Emoto <kemoto@codemonger.io>
2025-03-20 00:35:11 +0900

    feat(cdk): add SdkErrorExt implementations
    Implements `SdkErrorExt` for `E` = `QueryError`, and `ListUsersError`.
    Introduces a utility function `is_common_retryable_error` which tests if
    a given error is a common retryable error, "ServiceUnavailable", or
    "ThrottlingException".

--- cdk/lambda/authentication/src/sdk_error_ext.rs
+++ cdk/lambda/authentication/src/sdk_error_ext.rs
@@ -1,8 +1,12 @@
 //! Provides an extension for [`SdkError`](https://docs.rs/aws-smithy-runtime-api/latest/aws_smithy_runtime_api/client/result/enum.SdkError.html).
 
-use aws_sdk_dynamodb::operation::put_item::PutItemError;
+use aws_sdk_cognitoidentityprovider::operation::list_users::ListUsersError;
+use aws_sdk_dynamodb::operation::{
+    put_item::PutItemError,
+    query::QueryError,
+};
 use aws_smithy_runtime_api::client::result::SdkError;
-use aws_smithy_types::error::metadata::ProvideErrorMetadata as _;
+use aws_smithy_types::error::metadata::ProvideErrorMetadata;
 
 /// Extension for [`SdkError`](https://docs.rs/aws-smithy-runtime-api/latest/aws_smithy_runtime_api/client/result/enum.SdkError.html)
 /// which provides an additional test method.
@@ -20,12 +24,42 @@ impl<R> SdkErrorExt for SdkError<PutItemError, R> {
             SdkError::ServiceError(e) => match e.err() {
                 PutItemError::ProvisionedThroughputExceededException(_) |
                 PutItemError::RequestLimitExceeded(_) => true,
-                e => match e.code() {
-                    Some("ServiceUnavailable") | Some("ThrottlingException") => true,
-                    _ => false,
-                }
+                e => is_common_retryable_error(e),
             }
             _ => false,
         }
+    }
+}
+
+impl<R> SdkErrorExt for SdkError<QueryError, R> {
+    fn is_retryable(&self) -> bool {
+        match self {
+            SdkError::ServiceError(e) => match e.err() {
+                QueryError::ProvisionedThroughputExceededException(_) |
+                QueryError::RequestLimitExceeded(_) => true,
+                e => is_common_retryable_error(e),
+            }
+            _ => false,
+        }
+    }
+}
+
+impl<R> SdkErrorExt for SdkError<ListUsersError, R> {
+    fn is_retryable(&self) -> bool {
+        match self {
+            SdkError::ServiceError(e) => match e.err() {
+                ListUsersError::TooManyRequestsException(_) => true,
+                e => is_common_retryable_error(e),
+            }
+            _ => false,
+        }
+    }
+}
+
+/// Returns if a given error is a common retryable error.
+fn is_common_retryable_error(e: &impl ProvideErrorMetadata) -> bool {
+    match e.code() {
+        Some("ServiceUnavailable") | Some("ThrottlingException") => true,
+        _ => false,
     }
 }
